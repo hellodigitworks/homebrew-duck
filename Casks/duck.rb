0b31db46830988f43ba96afa5136a4268ae55292
swayam <swayam@Swayams-Mac.local>
2026-09-12 17:14:55 +0530

    Update Duck to version 1.0.1.
    Use the Duck 1.0.1 release archive and its verified checksum.

--- Casks/duck.rb
+++ Casks/duck.rb
@@ -1,8 +1,8 @@
 # Duck, for Homebrew. Written by scripts/make-cask.py in the Duck repo.
 # Do not edit by hand: make a release, then run that script again.
 cask "duck" do
-  version "1.0.0"
-  sha256 "310e8da84e8e74f832fd425f93e0897214c797fc2acb1fd7088b99d13cfcfe38"
+  version "1.0.1"
+  sha256 "a35d85d4f7d9371bdbf4514876b94211e2e903d461a9ba9ce33a5c35415aa965"
 
   url "https://github.com/hellodigitworks/Duck/releases/download/v#{version}/Duck.zip"
   name "Duck"
